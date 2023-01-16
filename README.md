506725b64c293c1c9bd0f5edbb4fd0d1b5896312
vijayakumar.ayyavoo@toyota.com <vijayakumar.ayyavoo@toyota.com>
2023-01-16 16:35:03 +0530

    added date util with future date check module

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# date-utils
+# Date Utils
+
+This repository keeps the util scripts to handle dates in python.
+
+## Checks whether given date is future date
+This CLI/util can be used to check given date is future date.
+
+This module takes below parameters.
+- date -- The date to check. Provide the date in format yyyy-MM-dd. e.g. 2023-01-16
+
+Refer more details 
+- [Python Date](https://docs.python.org/3/library/datetime.html)
+- [Comparing date in python](https://stackoverflow.com/questions/13227597/how-to-compare-dates-only-and-not-the-time-in-python)
---
+++ check_future_date.py
@@ -0,0 +1,38 @@
+"""This is the python module used to check whether the given date is future date.
+
+This module takes below parameters.
+date -- The date to check. Provide the date in format yyyy-MM-dd. e.g. 2023-01-16
+"""
+
+import datetime
+import argparse
+
+
+parser = argparse.ArgumentParser(
+    description='Checks the given date with current date and returns true if it is future date'
+)
+parser.add_argument(
+    '-d',
+    '--date',
+    required=True,
+    help='Provide the date to check (in format yyyy-MM-dd) e.g. 2023-01-16')
+
+args = parser.parse_args()
+input_date = args.date
+
+def is_future_date(input_date):
+    """Checks whether the given date is future date
+
+    Args:
+        input_date (str): date to check (in format yyyy-MM-dd) e.g. 2023-01-16
+
+    Returns:
+        bool: whether it is future date
+    """
+    # Cast specific date to Date Python Object
+    date = datetime.datetime.strptime(input_date, '%Y-%m-%d').date()
+    # Get today date with Date Python Object
+    today = datetime.date.today()
+    return date > today
+
+print (is_future_date(input_date))
